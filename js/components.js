43e70e780b635181a0309b2cb62d0e61003152b6
NeroLuna <nerolunakr@gmail.com>
2026-08-27 09:49:25 +0900

    🐛 모바일 버그 수정 1차

--- js/components.js
+++ js/components.js
@@ -6,14 +6,12 @@
     const isHome = currentPath.endsWith('index.html') || currentPath.endsWith('/') || currentPath === '';
     const isStore = currentPath.endsWith('store.html');
 
-    // 💡 상단 및 하단 그라데이션 오버레이가 포함된 고정 배경 주입
+    // 순수 배경 이미지만 주입
     const bgContainer = document.getElementById('common-bg');
     if (bgContainer) {
         bgContainer.innerHTML = `
             <div class="fixed-bg-wrapper">
                 <div class="custom-bg-img" id="customBg"></div>
-                <div class="top-gradient-overlay"></div>
-                <div class="bottom-gradient-overlay"></div>
             </div>
         `;
     }
